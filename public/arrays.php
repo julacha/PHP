74755364bf1bf190ce1c2237dc96f978ea1bea7e
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-18 12:52:47 +0200

    added arrays

--- public/arrays.php
+++ public/arrays.php
@@ -1,12 +1,27 @@
 <?php
-echo "<h1>Arrays </h1>";
-$myarr = [1, 2, 3, 4, 5, 6, 7, 8];
-for ($myarr as $val) {
-    echo "Nuber is $val<br>";
+echo "<h1>Arrays</h1>";
+$myarr = [1, 2, 5, 3, 32, 6, 6, 7, 7];
+foreach ($myarr as $val) {
+    echo "Number is $val<br>";
 }
-for each ($myarr as $key => $val) {
+
+foreach ($myarr as $key => $val) {
     echo "Key: $key is $val<br>";
 }
+
 $cars = [
-    "BWW"
-]
+    "BMW" => 525,
+    "VW" => "Golf",
+    "Lada" => "2001",
+
+];
+foreach ($cars as $key => $val) {
+    echo "Key: $key is $val<br>";
+}
+$cars["Citreon"] = "C9";
+var_dump($cars);
+$cars[5] = "Just a number";
+var_dump($cars);
+echo "<hr>";
+$myarr["myname"] = "Valdis";
+var_dump($myarr);
